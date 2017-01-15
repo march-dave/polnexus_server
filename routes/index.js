d3aeb77112115471ace4e27e5cac8d7872666333
march-dave <ysangy@gmail.com>
2017-01-14 21:50:09 -0500

    index router

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,15 @@ var router = express.Router();
 
 var path = require('path');
 
-router.get('/', (req, res) => {
-  var indexPath = path.join(__dirname, '../views/index.ejs');
-  res.sendFile(indexPath);
+// router.get('/', (req, res) => {
+//   var indexPath = path.join(__dirname, '../views/index.html');
+//   res.sendFile(indexPath);
+// });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
